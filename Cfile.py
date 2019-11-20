5df67c312065bfdc1b52056ae2b1e8583f51ad5c
SabriQ <196989054@qq.com>
2019-11-20 19:57:56 +0800

    add miniscope

--- Cfile.py
+++ Cfile.py
@@ -4,8 +4,9 @@ Created on Thu Oct 31 16:07:26 2019
 
 @author: Sabri
 """
-import os
+import os,re
 from Cvideo import Video
+import scipy.io as spio
 class File():
     def __init__ (self,file_path):
         self.file_path = file_path
@@ -20,7 +21,7 @@ class File():
         keep_origin = True，表示会复制原文件，否则是直接操作源文件
         '''
         if os.path.exists(self.file_path):
-            newname = os.path.join(self.dirname,prefix+'_'+self.file_name_noextension+"_"+suffix+self.extension)
+            newname = os.path.join(self.dirname,prefix+self.file_name_noextension+suffix+self.extension)
             if keep_origin:
                 from shutil import copyfile
                 copyfile(self.file_path,newname)
@@ -39,15 +40,34 @@ class File():
             print(f"Transfer {self.file_path} successfully")
         else:
             print("{self.file_path} does not exists.")
+
 class TrackFile(File):
     def __init__(self,file_path):
         super().__init__(file_path)
         self.trackvideo_path = os.path.join(self.dirname,self.file_name_noextension+'_labeled.mp4')
-    def restrict_area(self):
-        Video(self.trackvideo_path).draw_rois(aim="restrict",count = 1)
+
+class Matfile(File):
+    def __init__(self,file_path):
+        super().__init__(file_path)
+
+    def ms_traceview(self,num):
+        fig = plt.figure()
+        for i in range(num):
+            ax = fig.add_subplot(num,1,i+1)
+            ax.
+
+class Free2pFile(File):
+    def __init__(self,file_path):
+        super().__init__(file_path)
+
+
 if __name__ == "__main__":
 #    File(r"C:\Users\Sabri\Desktop\test\test_180228160127Cam-1_Test.asf").add_prefixAsuffix(prefix="test",suffix="Test",keep_origin=False)
 #    File(r"C:\Users\Sabri\Desktop\test\test_test_180228160127Cam-1_Test_Test.asf").copy2(r"C:\Users\Sabri\Desktop\test\tt")
-    TrackFile(r"C:\Users\Sabri\Desktop\test\trackfiles\191174A-20191110-221945DeepCut_resnet50_linear_track_40cm_ABSep26shuffle1_500000.h5").restrict_area()
+#    TrackFile(r"C:\Users\Sabri\Desktop\test\trackfiles\191174A-20191110-221945DeepCut_resnet50_linear_track_40cm_ABSep26shuffle1_500000.h5").restrict_area()
+    import glob
+    videolists = glob.glob(r"Z:\XuChun\Lab Projects\01_Intra Hippocampus\Miniscope_CFC\RawData\201910*\*\*\*1000000.csv")
 
+    for video in videolists:
+        File(video).add_prefixAsuffix(prefix = "behave_video_",suffix ="",keep_origin=True)
 
