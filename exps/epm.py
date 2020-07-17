7f21af8ec4dbaeba210dd446bc22f15eecb3d053
SabriQ <196989054@qq.com>
2020-07-17 10:43:18 +0800

    update

--- exps/epm.py
+++ exps/epm.py
@@ -1,29 +1,25 @@
 import sys,os
 import time
+import csv
+from mylab.exps.Cexps import *
+from matplotlib.pyplot import MultipleLocator
+import matplotlib.pyplot as plt
+import numpy as np
+
 class EPM(Exp):
-    def __init__(self,port,data_dir):
+    def __init__(self,port,data_dir=r"D:\EPM"):
         super().__init__(port,data_dir)
 
-    def __call__(self,mouse_id,data_dir="",):
-        self.mouse_id = mouse_id
-        video_name = "EPM_"+self.mouse_id+'-'+time.strftime("%Y%m%d-%H%M%S", time.localtime())+'.mp4'
-        self.video_path = os.path.join(video_dir,video_name)
+    def __call__(self,):
+        self.run()
 
-        self.run(app=app)
+    def optogenetics(self):
+        mouse_id = input("请输入mouse_id,并按Enter开始实验:")
+        self.mouse_id = str(mouse_id)
 
-    def run(self,app):
-        p = self.record_camera(self.video_path)
-        app()
-        self.stop_camera(p)
+        ## experimental process
 
-    def Epm_opto(self):     
-        '''
-        1 start the video recording
-        2 3min for arms exploration without laser
-        3 3min for arms exploration with laser, led on when laser on or off
-        4 3min for arms exploration without laser
-        5 3min for arms exploration with laser
-        ''' 
+        self.opencv_is_record()# start video record
         print(">>>without laser for 3 mins: ")
         self.ser.write("2".encode())
         self.countdown(180)
@@ -33,9 +29,28 @@ class EPM(Exp):
         print(">>>without laser for 3mins: ")
         self.ser.write('2'.encode())
         self.countdown(180)
+
+
+        self.opencv_is_record()# stop video record
+        if EPM.is_stop == 1:
+            return 0
+        else:
+            return self.retrieval_test()
         
-    
+    def run(self):
+        '''
+        '''
+        behave_fourcc = cv2.VideoWriter_fourcc(*'mpeg')
+        camera_behave = threading.Thread(target=self.play_video,args=(0,))
+        camera_behave_save = threading.Thread(target=self.save_video,args=(0,behave_fourcc,11,(640,480),))
+        exp = threading.Thread(target=self.optogenetics)
+        
+        camera_behave.start()
+        camera_behave_save.start()
+        exp.start()
+        print("main process is done!")
 
 if __name__ =="__main__":
-    epm = EPM()
-    epm()
+    lw = EPM(port=None)
+    lw()
+    # 画图测试
