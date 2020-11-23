306467c5f3e8707ef0ef818828c6cb47cf9d5379
SabriQ <196989054@qq.com>
2020-11-23 23:01:11 +0800

    update behave_stat_info

--- ana/miniscope/context_exposure/ana_funtions.py
+++ ana/miniscope/context_exposure/ana_funtions.py
@@ -584,28 +584,27 @@ def behave_stat_info(s,):
     stat_info = {}
     s.add_behave_choice_side()
     Choice_side = s.result["behave_choice_side"]
-    s.add_behave_forward_context(according="Enter_Context")
-    Context  = s.result["behave_forwad_context"]
+    s.add_behave_forward_context(according="Enter_ctx")
+    Context  = s.result["behave_forward_context"]
 
-    stat_info["video_name"] = s.result["behavevideo"][0]
+    video_name = s.result["behavevideo"][0]
     stat_info["date"] = re.findall(r"(\d{8})-\d{6}",video_name)[0]
     stat_info["mouse_id"] = re.findall(r"(\d+)-\d{8}-\d{6}",video_name)[0]
-    stat_info["aim"] = re.findall(r"CDC-(.*)-%s"%mouse_id,video_name)[0]
+    stat_info["aim"] = re.findall(r"CDC-(.*)-%s"%stat_info["mouse_id"],video_name)[0]
     stat_info["Trial_Num"] = s.result["behavelog_info"].shape[0]
-
 
 
     Left_choice = s.result["behavelog_info"]["Left_choice"]
     Right_choice = s.result["behavelog_info"]["Right_choice"]
     Choice_class = s.result["behavelog_info"]["Choice_class"]
-    forward_context = s.result["add_behave_forward_context"]
+    forward_context = s.result["behave_forward_context"]
+    if not sum(Left_choice)*sum(Right_choice) == 0:
+        stat_info["bias"] =  (max(Left_choice)-max(Right_choice))/(max(Left_choice)+max(Right_choice))
+        stat_info["Total_Accuracy"] = sum(Choice_class)/len(Choice_class)
+        stat_info["Left_Accuracy"] = sum(Choice_class[Choice_side=="left"])/len(Choice_class[Choice_side=="left"])
+        stat_info["Right_Accuracy"] = sum(Choice_class[Choice_side=="right"])/len(Choice_class[Choice_side=="right"])
 
-    state_info["bias"] =  (max(Left_choice)-max(Right_choice))/(max(Left_choice)+max(Right_choice))
-    state_info["Total_Accuracy"] = sum(Choice_class)/len(Choice_class)
-    state_info["Left_Accuracy"] = sum(Choice_class[Choice_side=="left"])/len(Choice_class[Choice_side=="left"])
-    state_info["Right_Accuracy"] = sum(Choice_class[Choice_side=="right"])/len(Choice_class[Choice_side=="right"])
+        for ctx in set(Context):
+            stat_info["ctx_%s_Accuracy"%ctx]= sum(Choice_class[Context==ctx])/len(Choice_class[Context==ctx])
 
-    for ctx in set(Context):
-        state_info["ctx_%s_Accuracy"%ctx]= sum(Choice_class[Context==ctx])/len(Choice_class[Context==ctx])
-
-    return state_info
+    return stat_info
