d78b5a962dfa0334ba4ebccdbce164958062a270
SabriQ <196989054@qq.com>
2020-11-27 13:29:41 +0800

    addapt cfc to datetime and ffmpeg

--- process/CFC/Ccfcfile.py
+++ process/CFC/Ccfcfile.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-import os,sys
-
+import os,sys,csv
+from mylab.Cfile import File
 class FreezingFile(File):
     def __init__(self,file_path):
         super().__init__(file_path)
@@ -25,7 +25,8 @@ class FreezingFile(File):
 
     def freezing_percentage(self,threshold = 0.005, start = 0, stop = 300,show_detail=False,percent =True,save_epoch=True): 
         data = pd.read_csv(self.file_path)
-        print(len(data['0']),"time points ;",len(data['Frame_No']),"frames")
+        print(data)
+        print(len(data['ts']),"time points ;",len(data['Frame_No']),"frames")
 
         data = data.dropna(axis=0)             
         #print(f"{self.file_path}")
@@ -36,23 +37,23 @@ class FreezingFile(File):
         if start>stop:
             start,stop = stop,start
             warning.warn("start time is later than stop time")
-        if start >=max(data['0']):
+        if start >=max(data['ts']):
             warnings.warn("the selected period start is later than the end of experiment")
             sys.exit()
-        elif start <=min(data['0']):            
+        elif start <=min(data['ts']):            
             start_index = 0
         else:
-            start_index = [i for i in range(len(data['0'])) if data['0'][i]<=start and  data['0'][i+1]>start][0]+1
+            start_index = [i for i in range(len(data['ts'])) if data['ts'][i]<=start and  data['ts'][i+1]>start][0]+1
 
         #stop_index
-        if stop >= max(data['0']):
-            stop_index = len(data['0'])-1
+        if stop >= max(data['ts']):
+            stop_index = len(data['ts'])-1
             warnings.warn("the selected period exceed the record time, automatically change to the end time.")
-        elif stop <=min(data['0']):
+        elif stop <=min(data['ts']):
             warnings.warn("the selected period stop is earlier than the start of experiment")
             sys.exit()
         else:            
-            stop_index = [i for i in range(len(data['0'])) if data['0'][i]<=stop and  data['0'][i+1]>stop][0]
+            stop_index = [i for i in range(len(data['ts'])) if data['ts'][i]<=stop and  data['ts'][i+1]>stop][0]
 
         selected_data = data.iloc[start_index:stop_index+1]
 
@@ -69,16 +70,16 @@ class FreezingFile(File):
             if value ==1:              
                 begin = sum(lengthes[0:i])
                 end = sum(lengthes[0:i+1])
-                if end > len(selected_data['0'])-1:
-                    end = len(selected_data['0'])-1
-                condition = selected_data['0'].iat[end]-selected_data['0'].iat[begin]
+                if end > len(selected_data['ts'])-1:
+                    end = len(selected_data['ts'])-1
+                condition = selected_data['ts'].iat[end]-selected_data['ts'].iat[begin]
                 if condition >=1:
                     if show_detail:
-                        print(f"{round(selected_data['0'].iat[begin],1)}s--{round(selected_data['0'].iat[end],1)}s,duration is {round(condition,1)}s".rjust(35,'-'))
+                        print(f"{round(selected_data['ts'].iat[begin],1)}s--{round(selected_data['ts'].iat[end],1)}s,duration is {round(condition,1)}s".rjust(35,'-'))
                     if save_epoch:
                         with open(self.freezingEpochPath,'a+',newline="") as csv_file:
                             writer = csv.writer(csv_file)
-                            writer.writerow([selected_data['0'].iat[begin],selected_data['0'].iat[end]])
+                            writer.writerow([selected_data['ts'].iat[begin],selected_data['ts'].iat[end]])
                     sum_freezing_time = sum_freezing_time + condition
                 else:
                     sum_freezing_time = sum_freezing_time
